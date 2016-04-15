9246319bcf6f12e46b030f45c1f2fa830e2c6a16
chaoqiang <tao-chqang@163.com>
2016-04-15 08:20:52 +0800

    fix reqtime parsing

--- hadoop/src/main/java/skyler/tao/hadoop/readwrite/HdfsReadWrite.java
+++ hadoop/src/main/java/skyler/tao/hadoop/readwrite/HdfsReadWrite.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -18,77 +17,90 @@ import org.apache.log4j.Logger;
 import org.json.JSONObject;
 
 public class HdfsReadWrite extends Configured implements Tool {
-    
-    public static final String FS_PARAM_NAME = "fs.defaultFS";
-    private static final Logger logger = Logger.getLogger(HdfsReadWrite.class);
-    
-    public int run(String[] args) throws Exception {
-        
-        if (args.length < 2) {
-            logger.info("HdfsReadWrite [hdfs input path] [hdfs output path]");
-            return 1;
-        }
-        
-        Path inputPath = new Path(args[0]);
-        Path outputPath = new Path(args[1]);
-        
-        Configuration conf = getConf();
-        logger.info("configured filesystem = " + conf.get(FS_PARAM_NAME));
-        FileSystem fs = FileSystem.get(conf);
-        BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(inputPath)));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fs.create(outputPath,false)));
-        
-        String message = null;
-        String messageProcessed = null;
-        try {
-        	while ((message = br.readLine())!= null) {
-        		
-        		StatsParser parser = new StatsParser(message);
-        		StatsHiveTarget target = new StatsHiveTarget();
-        		JSONObject json = parser.getJSONObject();
-        		target.setReqtime(json.optString("reqtime","null"));
-        		target.setReqid(json.optString("reqid","null"));
-        		target.setUid(json.optString("uid", "null"));
-        		target.setFrom(json.optString("from","null"));
-        		target.setPlatform(json.optString("platform","null"));
-        		target.setVersion(json.optString("version","null"));
-        		target.setIp(json.optString("ip","null"));
-        		target.setProxy_source(json.optString("proxy_source","null"));
-        		target.setWm(json.optString("wm","null"));
-        		target.setAvailable_pos(json.optString("available_pos","null"));
-        		target.setCategory_r(json.optString("category_r","null"));
-        		target.setIdfa(json.optString("idfa","null"));
-        		target.setImei(json.optString("imei","null"));
-        		target.setLocation(json.optString("location","null"));
-        		target.setIs_unread_pool(json.optString("is_unread_pool","null"));
-        		target.setLoadmore(json.optString("loadmore","null"));
-        		target.setFeedsnum(json.optString("feedsnum","null"));
-        		target.setUnread_status(json.optString("unread_status","null"));
-        		target.setLast_span(json.optString("last_span","null"));
-        		target.setProduct_r(json.optString("product_r","null"));
-        		target.setRefresh_type(json.optString("refresh_type","null"));
-        		target.setPostostock(json.optString("pos","null"));
-        		target.setTmeta(json.optString("tmeta_l2","null"));
-        		Timestamp timestamp = new Timestamp(json.optLong("reqtime"));
-        		Date date = new Date(timestamp.getTime());
-        		target.setStat_date(date.toString());
-        		target.setService_name(json.optString("service_name","null"));
-        		
-        		messageProcessed = target.toString();
-        		bw.write(messageProcessed);
-        	}
-            bw.flush();
-        } catch (Exception e) {
-        	
-        } finally {
-        	br.close();
-        	bw.close();
-        }
-        return 0;
-    }
-    
-    public static void main( String[] args ) throws Exception {
-        int returnCode = ToolRunner.run(new HdfsReadWrite(), args);
-        System.exit(returnCode);
-    }
+
+	public static final String FS_PARAM_NAME = "fs.defaultFS";
+	private static final Logger logger = Logger.getLogger(HdfsReadWrite.class);
+
+	public int run(String[] args) throws Exception {
+
+		if (args.length < 2) {
+			logger.info("HdfsReadWrite [hdfs input path] [hdfs output path]");
+			return 1;
+		}
+
+		Path inputPath = new Path(args[0]);
+		Path outputPath = new Path(args[1]);
+
+		Configuration conf = getConf();
+		logger.info("configured filesystem = " + conf.get(FS_PARAM_NAME));
+		FileSystem fs = FileSystem.get(conf);
+		BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(inputPath)));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fs.create(outputPath, false)));
+
+		String message = null;
+		try {
+			while ((message = br.readLine()) != null) {
+
+				bw.write(parseLine(message));
+			}
+			bw.flush();
+		} catch (Exception e) {
+
+		} finally {
+			br.close();
+			bw.close();
+		}
+		return 0;
+	}
+	
+	public String parseLine(String message) {
+		String result = "";
+		StatsParser parser = new StatsParser(message);
+		StatsHiveTarget target = new StatsHiveTarget();
+		JSONObject json = parser.getJSONObject();
+		target.setReqtime(json.optString("reqtime", "null"));
+		target.setReqid(json.optString("reqid", "null"));
+		target.setUid(json.optString("uid", "null"));
+		target.setFrom(json.optString("from", "null"));
+		target.setPlatform(json.optString("platform", "null"));
+		target.setVersion(json.optString("version", "null"));
+		target.setIp(json.optString("ip", "null"));
+		target.setProxy_source(json.optString("proxy_source", "null"));
+		target.setWm(json.optString("wm", "null"));
+		target.setAvailable_pos(json.optString("available_pos", "null"));
+		target.setCategory_r(json.optString("category_r", "null"));
+		target.setIdfa(json.optString("idfa", "null"));
+		target.setImei(json.optString("imei", "null"));
+		target.setLocation(json.optString("location", "null"));
+		target.setIs_unread_pool(json.optString("is_unread_pool", "null"));
+		target.setLoadmore(json.optString("loadmore", "null"));
+		target.setFeedsnum(json.optString("feedsnum", "null"));
+		target.setUnread_status(json.optString("unread_status", "null"));
+		target.setLast_span(json.optString("last_span", "null"));
+		target.setProduct_r(json.optString("product_r", "null"));
+		target.setRefresh_type(json.optString("refresh_type", "null"));
+		target.setPostostock(json.optString("pos", "null"));
+		target.setTmeta(json.optString("tmeta_l2", "null"));
+		target.setStat_date(reqtime2date(json.optString("reqtime")));
+		target.setService_name(json.optString("service_name", "null"));
+
+		result = target.toString();
+		return result;
+	}
+
+	private String reqtime2date(String reqtime) {
+		String result = "";
+		if (reqtime.length() == 10) {
+			long millis = Long.parseLong(reqtime) * 1000;
+			Date date = new Date(millis);
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+			result = format.format(date).toString();
+		}
+		return result;
+	}
+
+	public static void main(String[] args) throws Exception {
+		int returnCode = ToolRunner.run(new HdfsReadWrite(), args);
+		System.exit(returnCode);
+	}
 }
--- hadoop/src/test/java/skyler/tao/hadoop/readwrite/HdfsReadWriteTest.java
+++ hadoop/src/test/java/skyler/tao/hadoop/readwrite/HdfsReadWriteTest.java
@@ -2,21 +2,21 @@ package skyler.tao.hadoop.readwrite;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
-import org.json.JSONObject;
 import org.junit.Test;
 
 public class HdfsReadWriteTest {
 
-	public static final String L1S = "\\x1A";
-	public static final String L1CS = "\\x1C";
+//	public static final String L1S = "\\x1A";
+//	public static final String L1CS = "\\x1C";
 
 	@Test
-	public void parseTmeta() throws Exception {
+	public void parseLineTest() throws Exception {
 		
 		String file = "stats_log_sample.log";
 	    BufferedReader reader = null;
@@ -24,38 +24,10 @@ public class HdfsReadWriteTest {
 	    try {
 	        reader = new BufferedReader(new FileReader(file));
 	        String message = null;
+	        HdfsReadWrite targetClass = new HdfsReadWrite();
 	        while ((message = reader.readLine()) != null) {
-	        	StatsParser parser = new StatsParser(message);
-        		StatsHiveTarget target = new StatsHiveTarget();
-        		JSONObject json = parser.getJSONObject();
-        		target.setReqtime(json.optString("reqtime","null"));
-        		target.setReqid(json.optString("reqid","null"));
-        		target.setUid(json.optString("uid", "null"));
-        		target.setFrom(json.optString("from","null"));
-        		target.setPlatform(json.optString("platform","null"));
-        		target.setVersion(json.optString("version","null"));
-        		target.setIp(json.optString("ip","null"));
-        		target.setProxy_source(json.optString("proxy_source","null"));
-        		target.setWm(json.optString("wm","null"));
-        		target.setAvailable_pos(json.optString("available_pos","null"));
-        		target.setCategory_r(json.optString("category_r","null"));
-        		target.setIdfa(json.optString("idfa","null"));
-        		target.setImei(json.optString("imei","null"));
-        		target.setLocation(json.optString("location","null"));
-        		target.setIs_unread_pool(json.optString("is_unread_pool","null"));
-        		target.setLoadmore(json.optString("loadmore","null"));
-        		target.setFeedsnum(json.optString("feedsnum","null"));
-        		target.setUnread_status(json.optString("unread_status","null"));
-        		target.setLast_span(json.optString("last_span","null"));
-        		target.setProduct_r(json.optString("product_r","null"));
-        		target.setRefresh_type(json.optString("refresh_type","null"));
-        		target.setPostostock(json.optString("pos","null"));
-        		target.setTmeta(json.optString("tmeta_l2","null"));
-        		Timestamp timestamp = new Timestamp(json.optLong("reqtime"));
-        		Date date = new Date(timestamp.getTime());
-        		target.setStat_date(date.toString());
-        		target.setService_name(json.optString("service_name","null"));
-	    		System.out.println(target.toString());
+	        	String parsedLine = targetClass.parseLine(message);
+	    		System.out.println(parsedLine);
 	        }
 	    } catch (Exception e) {
 	    	System.out.println("ERROR!");
@@ -65,18 +37,21 @@ public class HdfsReadWriteTest {
 	    }
 	}
 	
-//	@Test
+	@Test
 	public void reqtime2dateTest() {
 		String reqtime = "1460629505";
-		System.out.println(Long.parseLong(reqtime));
-//		Timestamp timestamp = new Timestamp(Long.parseLong(reqtime));
-//		System.out.println(timestamp.toString());
-		Calendar start = Calendar.getInstance();
-		start.setTimeInMillis(Long.parseLong(reqtime));
-		System.out.println(start.getWeekYear());
-		Date date = start.getTime();
-//		System.out.println(date.toString());
-		SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-DD");
-//		System.out.println(format.format(date).toString());
+		long millis = Long.parseLong(reqtime) * 1000;
+		Date date = new Date(millis);
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println(format.format(date).toString());
+	}
+	
+	@Test
+	public void reqtime2date2() {
+		String reqtime = "1460629505";
+		long millis = Long.parseLong(reqtime) * 1000;
+		Instant instant = Instant.ofEpochMilli(millis);
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		System.out.println(fmt.format(instant.atZone(ZoneId.systemDefault())));
 	}
 }
