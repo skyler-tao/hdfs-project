4fa53e4a302f910ef501f444fb7e7f509ad326a5
chaoqiang <chaoqiang@staff.weibo.com>
2016-04-18 18:21:52 +0800

    temp

--- hadoop/src/main/java/skyler/tao/hadoop/HdfsReadDirectory.java
+++ hadoop/src/main/java/skyler/tao/hadoop/HdfsReadDirectory.java
@@ -1,5 +1,14 @@
 package skyler.tao.hadoop;
 
-public class HdfsReadDirectory {
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class HdfsReadDirectory extends Configured implements Tool {
+
+	@Override
+	public int run(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
