84966a83e996919789d3509390ab97cfe247844a
skyler2tao <tao-chqang@163.com>
2016-04-22 21:23:15 +0800

    add Read Directory

--- hadoop/src/main/java/skyler/tao/hadoop/HdfsReadDirectory.java
+++ hadoop/src/main/java/skyler/tao/hadoop/HdfsReadDirectory.java
@@ -1,13 +1,39 @@
 package skyler.tao.hadoop;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.util.Tool;
 
 public class HdfsReadDirectory extends Configured implements Tool {
 
+	public static final String FS_PARAM_NAME = "fs.defaultFS";
+
 	@Override
 	public int run(String[] args) throws Exception {
-		// TODO Auto-generated method stub
+
+		if (args.length < 2) {
+			System.err.println("HdfsReader [hdfs input path] [local output path]");
+			return 1;
+		}
+
+		Path inputPath = new Path(args[0]);
+		String localOutputPath = args[1];
+		Configuration conf = getConf();
+		System.out.println("configured filesystem = " + conf.get(FS_PARAM_NAME));
+		Job job = Job.getInstance(conf);
+		FileSystem fs = FileSystem.get(conf);
+		RemoteIterator<LocatedFileStatus> fileStatusListIterator = fs.listFiles(inputPath, true);
+		while (fileStatusListIterator.hasNext()) {
+			LocatedFileStatus fileStatus = fileStatusListIterator.next();
+			// do stuff with the file like ...
+			job.addFileToClassPath(fileStatus.getPath());
+		}
+
 		return 0;
 	}
 
